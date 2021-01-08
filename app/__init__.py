728986bf709e2600747600e9ce33793b8caf0242
austin <pachano.austin@gmail.com>
2021-01-08 15:26:03 -0800

    Switch to venv

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__, instance_relative_config=True)
 
-from app import views
-
 app.config.from_object('config')
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
