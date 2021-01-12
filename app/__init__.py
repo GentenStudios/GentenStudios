e8286a7307d9392807f065b22827045e0c2907e2
austin <pachano.austin@gmail.com>
2021-01-11 23:47:09 -0800

    Implement user login flow and sessions

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, session, redirect, url_for, request, render_template
+from markupsafe import escape
 
 app = Flask(__name__, instance_relative_config=True)
 
 app.config.from_object('config')
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+app.debug = True
 
 
 @app.route('/')
@@ -13,4 +15,27 @@ def index():
 
 @app.route('/about')
 def about():
-    return render_template("about.html")
+    if 'username' in session:
+        return 'Logged in as %s' % escape(session['username'])
+    return 'You are not logged in'
+    # return render_template("about.html")
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return '''
+        <form method="post">
+            <p><input type=text name=username>
+            <p><input type=submit value=Login>
+        </form>
+    '''
+
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
--- config.py
+++ config.py
@@ -1,1 +1,5 @@
-DEBUG = True
+class Config(object):
+    DEBUG = True
+    PG_PASS = 'EazdJhQW4j'
+    PG_USER = 'genten'
+    PG_URL = '/var/run/postgresql:5432'
